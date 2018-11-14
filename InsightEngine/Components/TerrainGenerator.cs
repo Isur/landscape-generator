c376ec50d37e002fe28b945226a982d1c192f12e
Dawid Grajewski <dgrajewski@evertop.pl>
2018-11-14 12:38:38 +0100

    Podstawy silnika Insight Engine;

---
+++ InsightEngine/Components/TerrainGenerator.cs
@@ -0,0 +1,221 @@
+﻿using InsightEngine.Contract;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System;
+using System.Drawing;
+
+namespace InsightEngine.Components
+{
+    public class TerrainGenerator : Component
+    {
+        public int Width { get; set; } = 100;
+        public int Lenght { get; set; } = 100;
+        public bool UseColors { get; set; } = true;
+
+        int vertCount { get; set; }
+        int indCount { get; set; }
+
+        CustomVertex.PositionColored[] verts { get; set; } = null;
+        int[] indices { get; set; } = null;
+
+        VertexBuffer vertexBuffer { get; set; } = null;
+        IndexBuffer indexBuffer { get; set; } = null;
+
+
+        public override void Start()
+        {
+            vertCount = Width * Lenght;
+            indCount = (Width - 1) * (Lenght - 1) * 6;
+
+            GenerateTerrain();
+
+            CreateEvents();
+        }
+
+        public override void Update()
+        {
+            device.SetStreamSource(0, vertexBuffer, 0); //tells the device where to receive the vertex information from
+            device.Indices = indexBuffer; //tells the device where to receive the index information from
+            device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, vertCount, 0, indCount / 3); //draws the triangles depending on the style we set it using the information from indices
+        }
+
+        private void GenerateTerrain()
+        {
+            GenerateVertex();
+            GenerateIndex();
+
+            vertexBuffer = new VertexBuffer(typeof(CustomVertex.PositionColored), vertCount, device, Usage.Dynamic | Usage.WriteOnly, CustomVertex.PositionColored.Format, Pool.Default);
+            OnVertexBufferCreate(vertexBuffer, null);
+
+            indexBuffer = new IndexBuffer(typeof(int), indCount, device, Usage.WriteOnly, Pool.Default);
+            OnIndexBufferCreate(indexBuffer, null);
+        }
+
+        private void CreateEvents()
+        {
+            vertexBuffer.Created += new EventHandler(OnVertexBufferCreate);
+            indexBuffer.Created += new EventHandler(OnIndexBufferCreate);
+        }
+
+        private void GenerateVertex()
+        {
+            verts = new CustomVertex.PositionColored[vertCount];
+            var perlin = new PerlinNoise();
+            int k = 0;
+
+            var perlinVerts = new float[Width, Lenght];
+            var min = 0f;
+            var max = 0f;
+
+            // gererating array of heights, highest point and lowest point
+            for (int z = 0; z < Width; z++)
+            {
+                for (int x = 0; x < Lenght; x++)
+                {
+                    var y = perlin.Get(x / 7f, z / 7f) * 20;
+                    perlinVerts[x, z] = y;
+
+                    if (y < min) min = y;
+                    if (y > max) max = y;
+                }
+            }
+
+            // initialize vertexes
+            for (int z = 0; z < Width; z++)
+            {
+                for (int x = 0; x < Lenght; x++)
+                {
+                    var y = perlinVerts[x, z];
+                    verts[k].Position = new Vector3(x, y, z);
+
+                    int color = Color.Blue.ToArgb();
+                    if (UseColors)
+                    {
+                        if (y > 0)
+                            color = Color.FromArgb(255, (int)map(y, 0, max, 255, 0), 0).ToArgb();
+                        else
+                            color = Color.FromArgb((int)map(y, min, 0, 0, 255), 255, 0).ToArgb();
+                    }
+                    else
+                    {
+                        color = Color.FromArgb(0, 0, (int)map(y, min, max, 185, 255)).ToArgb();
+                    }
+
+                    verts[k].Color = color;
+                    k++;
+                }
+            }
+
+        }
+
+        float map(float s, float a1, float a2, float b1, float b2)
+        {
+            return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+        }
+
+        private void GenerateIndex() // Generating all the indices vor the Index buffer (Her we tell how to connect the vertices to creat triangles)
+        {
+            indices = new int[indCount];
+
+            int k = 0;
+            int l = 0;
+            for (int i = 0; i < indCount; i += 6)
+            {
+                indices[i] = k;                     //
+                indices[i + 1] = k + Lenght;     // left triangle of the square
+                indices[i + 2] = k + Lenght + 1; //
+
+                indices[i + 3] = k;                   //
+                indices[i + 4] = k + Lenght + 1;   // right triangle of the square
+                indices[i + 5] = k + 1;               //
+                k++;
+                l++;
+                if (l == Lenght - 1) //once one line of rectangles is created and the end of the line is beeing reached skip one k
+                {
+                    l = 0;
+                    k++;
+                }
+            }
+        }
+
+        private void OnIndexBufferCreate(object sender, EventArgs e)
+        {
+            IndexBuffer buffer = (IndexBuffer)sender;
+            buffer.SetData(indices, 0, LockFlags.None); //puts all indices from the int array into the index buffer
+        }
+
+        private void OnVertexBufferCreate(object sender, EventArgs e)
+        {
+            VertexBuffer buffer = (VertexBuffer)sender;
+            buffer.SetData(verts, 0, LockFlags.None); //puts all vertices from the vertex array into the vertex buffer
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class PerlinNoise
+    {
+        float[,,] gradient = new float[512, 512, 2];
+        int SIZE = 100;
+
+        public PerlinNoise()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        gradient[i, j, k] = rand.Next(1, 100) / 100.0f;
+                    }
+                }
+            }
+        }
+
+        float lerp(float a0, float a1, float w)
+        {
+            return a0 + w * (a1 - a0);
+        }
+        float dotGridGradient(int ix, int iy, float x, float y)
+        {
+            float dx = x - (float)ix;
+            float dy = y - (float)iy;
+
+            return (dx * gradient[iy, ix, 0] + dy * gradient[iy, ix, 1]);
+        }
+
+        public float Get(float x, float y)
+        {
+            int x0 = (int)x;
+            int x1 = x0 + 1;
+            int y0 = (int)y;
+            int y1 = y0 + 1;
+
+            float sx = x - (float)x0;
+            float sy = y - (float)y0;
+
+            float n0, n1, ix0, ix1, value;
+            n0 = dotGridGradient(x0, y0, x, y);
+            n1 = dotGridGradient(x1, y0, x, y);
+            ix0 = lerp(n0, n1, sx);
+            n0 = dotGridGradient(x0, y1, x, y);
+            n1 = dotGridGradient(x1, y1, x, y);
+            ix1 = lerp(n0, n1, sx);
+            value = lerp(ix0, ix1, sy);
+
+            return value;
+        }
+    }
+}
