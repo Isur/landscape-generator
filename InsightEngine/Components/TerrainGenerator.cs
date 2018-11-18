cd672e5b37db080b34026564e18d30070ae75ff4
Dawid Grajewski <dgrajewski@evertop.pl>
2018-11-18 12:17:14 +0100

    Connected engine to perlin library;

--- InsightEngine/Components/TerrainGenerator.cs
+++ InsightEngine/Components/TerrainGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using InsightEngine.Contract;
 using Microsoft.DirectX;
 using Microsoft.DirectX.Direct3D;
+using PerlinNoise;
 using System;
 using System.Drawing;
 
@@ -63,7 +64,7 @@ namespace InsightEngine.Components
         private void GenerateVertex()
         {
             verts = new CustomVertex.PositionColored[vertCount];
-            var perlin = new PerlinNoise();
+            var perlin = new SimplePerlinNoise(Width, 2);
             int k = 0;
 
             var perlinVerts = new float[Width, Lenght];
@@ -76,7 +77,7 @@ namespace InsightEngine.Components
             {
                 for (int x = 0; x < Lenght; x++)
                 {
-                    var y = perlin.Get(x / Devider, z / Devider);
+                    var y = perlin.CalculatePerlin(x / Devider, z / Devider);
                     if (y > 0)
                         y *= rand.Next(1, 60);
                     else
@@ -157,74 +158,6 @@ namespace InsightEngine.Components
         {
             VertexBuffer buffer = (VertexBuffer)sender;
             buffer.SetData(verts, 0, LockFlags.None); //puts all vertices from the vertex array into the vertex buffer
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    class PerlinNoise
-    {
-        float[,,] gradient = new float[512, 512, 2];
-        int SIZE = 100;
-
-        public PerlinNoise()
-        {
-            Random rand = new Random();
-            for (int i = 0; i < SIZE; i++)
-            {
-                for (int j = 0; j < SIZE; j++)
-                {
-                    for (int k = 0; k < 2; k++)
-                    {
-                        gradient[i, j, k] = rand.Next(1, 100) / 100.0f;
-                    }
-                }
-            }
-        }
-
-        float lerp(float a0, float a1, float w)
-        {
-            return a0 + w * (a1 - a0);
-        }
-        float dotGridGradient(int ix, int iy, float x, float y)
-        {
-            float dx = x - (float)ix;
-            float dy = y - (float)iy;
-
-            return (dx * gradient[iy, ix, 0] + dy * gradient[iy, ix, 1]);
-        }
-
-        public float Get(float x, float y)
-        {
-            int x0 = (int)x;
-            int x1 = x0 + 1;
-            int y0 = (int)y;
-            int y1 = y0 + 1;
-
-            float sx = x - (float)x0;
-            float sy = y - (float)y0;
-
-            float n0, n1, ix0, ix1, value;
-            n0 = dotGridGradient(x0, y0, x, y);
-            n1 = dotGridGradient(x1, y0, x, y);
-            ix0 = lerp(n0, n1, sx);
-            n0 = dotGridGradient(x0, y1, x, y);
-            n1 = dotGridGradient(x1, y1, x, y);
-            ix1 = lerp(n0, n1, sx);
-            value = lerp(ix0, ix1, sy);
-
-            return value;
         }
     }
 }
