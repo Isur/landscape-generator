78e6701e98d119b1524bf78c9325197db1def6c9
jkthomas <jkthomas>
2018-12-19 14:31:18 +0100

    [PARAMS] Adjusted Perlin parameters to best possible. Added colors transition for grass texture.

--- PerlinNoise/Config/PerlinParameters.cs
+++ PerlinNoise/Config/PerlinParameters.cs
@@ -11,14 +11,14 @@ namespace PerlinNoise.Config
             //<Scale, ScaleWeight>
             Scales = new Dictionary<float, float>()
             {
-                {4.3f, 1f},
+                {4f, 1f},
                 {2.5f, 0.5f},
                 {50f, 0.01f}
             };
         }
 
         internal static readonly Dictionary<float, float> Scales;
-        internal const float power = 5f;
+        internal const float power = 5.25f;
         internal const float offsetX = 100f;
         internal const float offsetZ = 100f;
     }
